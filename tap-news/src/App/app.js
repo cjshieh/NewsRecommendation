d3ff19c44b050b71918976bb4f663feb6349096b
cjshieh <chingjshieh@gmail.com>
2018-08-27 10:06:53 +0800

    Style navbar

--- tap-news/src/App/app.js
+++ tap-news/src/App/app.js
@@ -1,12 +1,13 @@
-
-import React, { Component } from "react";
-import logo from './Logo.png';
 import './app.css';
+import logo from './Logo.png';
+import Nabbar from '../navbar/navbar';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
         <div>
+            <Nabbar />
             <img className='logo' src={logo} alt='logo' />
             <div className='container'></div>
         </div>
--- tap-news/src/Navbar/navbar.js
+++ tap-news/src/Navbar/navbar.js
@@ -1,32 +1,47 @@
 // import "../../node_modules/materialize-css/dist/css/materialize.css";
 
 import React, { Component } from "react";
+import Logo from "../assets/Logo.png";
 
 class NavBar extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#!" className="brand-logo">
-            Logo
-          </a>
-          {/*
-          <form>
-            <div className="input-field">
-              <input id="search" type="search" />
-              <label className="label-icon" htmlFor="search">
-                <i className="material-icons">search</i>
-              </label>
-              <i className="material-icons">close</i>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand mr-10" href="#">
+          <img src={Logo} width="150" height="42" />
+        </a>
+        {/*
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        */}
+
+        <form>
+          <div className="form-row align-items-center d-flex justify-content-center">
+            <div className="col-md-8 col-sm-4 pr-0 col-6">
+              <input
+                type="text"
+                className="form-control"
+                id="inlineFormInputName"
+                placeholder="Search"
+              />
             </div>
-          </form>
-          */}
-          <ul id="nav-mobile" className="right">
-            <li>
-              <a href="#!">Login</a>
-            </li>
-          </ul>
-        </div>
+            <div className="col-auto my-1 pl-0">
+              <button type="submit" className="btn btn-secondary">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent" />
       </nav>
     );
   }
