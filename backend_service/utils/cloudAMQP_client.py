1a88b38c0926eebc1eeae51e83e9aabd3938e642
cjshieh <chingjshieh@gmail.com>
2018-08-30 17:04:20 +0800

    Test cloudAMQP functionality

--- backend_service/utils/cloudAMQP_client.py
+++ backend_service/utils/cloudAMQP_client.py
@@ -9,9 +9,36 @@ class CloudAMQPClient:
     def __init__(self, cloud_amqp_url, queue_name):
         self.cloud_amqp_url = cloud_amqp_url
         self.queue_name = queue_name
-        # 
         self.params = pika.URLParameters(cloud_amqp_url)
         self.params._socket_timeout = 3
+        # The above setting is for the following code
         self.connection = pika.BlockingConnection(self.params)
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=queue_name)
+
+    # send a message
+    def sendMessage(self, message):
+        self.channel.basic_publish(exchange='', 
+                                   routing_key=self.queue_name,
+                                   body=json.dumps(message))
+        print "[x] Sent message to %s: %s" % (self.queue_name, message)
+    
+    # get a message
+    def getMessage(self):
+        # check whether the message is empty
+        # we need to parse from header
+        method_frame, header_frame, body = self.channel.basic_get(self.queue_name)
+        if method_frame:
+            print "[x] Received message from %s: %s" % (self.queue_name, body)
+            # delivery_tag to prevent others deliberatly remove queue
+            self.channel.basic_ack(method_frame.delivery_tag)
+            return json.loads(body)
+        else:
+            print "No message returned."
+            # Same as return null
+            return None
+
+    # Herat rate to check whether producer and consumer is still alive
+    # BlockingConnection.sleep is a safer way to sleep than time.sleep()
+    def sleep(self, seconds):
+        self.connection.sleep(seconds)
---
+++ backend_service/utils/cloudAMQP_client_test.py
@@ -0,0 +1,17 @@
+from cloudAMQP_client import CloudAMQPClient
+
+CLOUDAMQP_URL = "amqp://zzgxkgkb:m18Sp4SQjW-gG7hui9SyOKnUCg7fgwBu@mustang.rmq.cloudamqp.com/zzgxkgkb"
+TEST_QUEUE_NAME = "test"
+
+def test_basic():
+    client = CloudAMQPClient(CLOUDAMQP_URL, TEST_QUEUE_NAME)
+    
+    sentMsg = {"test":"test"}
+    client.sendMessage(sentMsg)
+    receivedMsg = client.getMessage()
+
+    assert sentMsg == receivedMsg
+    print "test_basic passed."
+
+if __name__ == "__main__":
+    test_basic()
