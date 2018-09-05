2087e40463252203852e5ad0fd68d4af33edbfdb
cjshieh <chingjshieh@gmail.com>
2018-09-06 00:34:36 +0800

    Auto running news pipeline

---
+++ news_pipeline_launcher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Start redis and mongoDB
+# redis-server /usr/local/etc/redis.conf
+# mongod --dbpath ~/data/db
+
+# install requirements
+sudo python3 -m pip install -r requirements.txt
+
+# run newspipeline
+cd news_pipline
+python3 news_deduper.py &
+python3 news_fetcher.py &
+python3 news_deduper.py &
+
+echo "=================================================="
+read -p "PRESS [ANY KEY] TO TERMINATE PROCESSES." PRESSKEY
+
+kill $(jobs -p)
