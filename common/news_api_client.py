14e51e353783c243b8c87633ce7cdf34a62e0c66
cjshieh <chingjshieh@gmail.com>
2018-09-06 17:54:17 +0800

    Extract more news

--- common/news_api_client.py
+++ common/news_api_client.py
@@ -1,10 +1,13 @@
 import requests
 
 from json import loads
+import math
 
 NEWS_API_PREFIX = 'https://newsapi.org/v2/'
 NEW_API_KEY = 'e847d9e615e644ad91b077336c66493c'
-
+NEWS_PER_PAGE = 100
+PAGE_START = 1
+LIMITS_OF_PAGES = 100
 TOP_NEWS_API = 'top-headlines'
 
 BBC_NEWS = 'bbc-news'
@@ -14,27 +17,47 @@ CNN = 'cnn'
 DEFAULT_SOURECES = [CNN, BBC_NEWS]
 
 def concatSources(sources):
-    return ','.join(sources) 
+    return ','.join(sources)
 
 def buildURL(endPoint=NEWS_API_PREFIX, apiMethod=TOP_NEWS_API):
     return endPoint + apiMethod
 
+def extractMoreNews(article, url, payload, number_of_result):
+    totalPages = math.ceil(number_of_result / NEWS_PER_PAGE)
+    totalPages = LIMITS_OF_PAGES if totalPages > LIMITS_OF_PAGES else totalPages
+    for page in range(PAGE_START, totalPages+1):
+        payload['page'] = str(page)
+        res_json, goodToProcess = requestFromUrl(url, payload)
+        if(goodToProcess):
+            article.extend(res_json['articles'])
+
+def requestFromUrl(url, payload):
+    response = requests.get(url, params=payload)
+    res_json = loads(response.content)
+    goodToProcess = False
+    if (res_json is not None and
+            res_json['status'] == 'ok'):
+            goodToProcess = True
+    return res_json, goodToProcess
+        
+
 def getNewsFromSource(sources=DEFAULT_SOURECES):
     acticles = []
-
+    sourceURL = buildURL()
+    allSources = concatSources(sources)
     payload = {
         'apiKey': NEW_API_KEY,
-        'sources': concatSources(sources),
-        'pageSize': '100'
+        'sources': allSources,
+        'pageSize': str(NEWS_PER_PAGE),
+        'page': str(PAGE_START)
     }
-    response = requests.get(buildURL(), params=payload)
-    res_json = loads(response.content)
-
-    # Extract info from response
-    if (res_json is not None and 
-        res_json['status'] == 'ok'):
-
-        acticles.extend(res_json['articles'])
     
+    res_json, goodToProcess = requestFromUrl(sourceURL, payload)
+    # Extract info from response
+    if(goodToProcess):
+        if res_json['totalResults'] > NEWS_PER_PAGE:
+            extractMoreNews(acticles, sourceURL, payload, res_json['totalResults'])
+        else:
+            acticles.extend(res_json['articles'])
+
     return acticles
-        
--- common/news_api_client_test.py
+++ common/news_api_client_test.py
@@ -1,13 +1,28 @@
 
-import news_api_client as client 
+import news_api_client as client
+
 
 def test_basic():
     news = client.getNewsFromSource()
-    print news
+    NEWS_SOURCES = [
+        'bbc-news',
+        'bbc-sport',
+        'bloomberg',
+        'cnn',
+        'entertainment-weekly',
+        'espn',
+        'ign',
+        'techcrunch',
+        'the-new-york-times',
+        'the-wall-street-journal',
+        'the-washington-post'
+    ]
+    print(news)
     assert len(news) > 0
-    news = client.getNewsFromSource(sources=['cnn', 'bbc-sport', 'bbc-news'])
+    news = client.getNewsFromSource(sources=NEWS_SOURCES)
     assert len(news) > 0
-    print '[x] test basic new_api passed!'
+    print('[x] test basic new_api passed!')
+
 
 if __name__ == "__main__":
     test_basic()
