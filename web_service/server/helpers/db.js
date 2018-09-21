cd472c8262159888e10c610ff4d68891bbf348a6
cjshieh <chingjshieh@gmail.com>
2018-09-21 22:58:16 +0800

    Connect auth db

---
+++ web_service/server/helpers/db.js
@@ -0,0 +1,9 @@
+const config = require('config.json');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.connectionString);
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    User: require('../users/user_model')
+}
