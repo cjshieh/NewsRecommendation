c7dc02960c45d8d3b8c21eb5441c3eb2b4a33371
cjshieh <chingjshieh@gmail.com>
2018-09-26 01:28:38 +0800

    Remove printing log

--- web_service/server/routes/user_controller.js
+++ web_service/server/routes/user_controller.js
@@ -13,7 +13,6 @@ router.put('/:id', update);
 module.exports = router;
 
 function authenticate(req, res, next) {
-    console.log(req.body);
     userService.authenticate(req.body)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
         .catch(err => next(err));
