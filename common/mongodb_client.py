762efc0189022cb9a733191bdc4c60e9b9ac452d
cjshieh <chingjshieh@gmail.com>
2018-10-24 00:42:01 +0800

    Change db path

--- common/mongodb_client.py
+++ common/mongodb_client.py
@@ -2,11 +2,10 @@ from pymongo import MongoClient
 import config_reader as reader
 
 config = reader.read_config()
-MONGO_DB_HOST = config.get('DB', 'MONGO_DB_HOST')
-MONGO_DB_PORT = config.get('DB', 'MONGO_DB_PORT')
+MONGO_DB_URI = config.get('DB', 'MONGO_DB_URI')
 DB_NAME = config.get('DB', 'DB_NAME')
 
-client = MongoClient("%s:%s" % (MONGO_DB_HOST, MONGO_DB_PORT))
+client = MongoClient(MONGO_DB_URI)
 
 def get_db(db=DB_NAME):
     return client[db]
